02762e0a96f6364fac9c2769f1c920942e33d0bc
Meha Mary Samuvel <mehasamuvel@gmail.com>
2026-03-04 03:13:53 +0530

    Final clean structure

--- app.py
+++ app.py
@@ -36,5 +36,9 @@ def register():
         return redirect(url_for("login"))
     return render_template("register.html")
 
+
+    import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
